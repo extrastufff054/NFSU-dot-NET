df2eae287beedf375fc4a79dfae7792d6026166a
extrastufff054 <extrastuff054@gmail.com>
2023-06-21 15:48:31 +0530

    26-07-2023

--- dot-NET/P8.cs
+++ dot-NET/P8.cs
@@ -1,26 +1,35 @@
-// //Write a program to find the smallest element from the given array
-
+// // Purpose: To find the smallest element of a matrix
 // using System;
 
-// namespace Smallest{
-//     class Smallest{
-//         static void Main()
-//         {
-//             int[] arr = new int[5];
-//             Console.WriteLine("Enter the elements of the array :");
-//             for(int i = 0; i < arr.Length; i++)
-//             {
-//                 arr[i] = int.Parse(Console.ReadLine());
-//             }
-//             int min = arr[0];
-//             for(int i = 0; i < arr.Length; i++)
-//             {
-//                 if(arr[i] < min)
-//                 {
-//                     min = arr[i];
-//                 }
-//             }
-//             Console.WriteLine("The smallest element of the array is :"+min);
-//         }
-//     }
+// namespace Smallest_Element_Matrix
+// {
+// 	class Smallest_Element_Matrix
+// 	{
+// 		static void Main()
+// 		{
+// 			int[,] matrix = new int[3,3];	//2D array
+// 			Console.WriteLine("Enter the elements of the matrix");
+// 			for(int i = 0 ; i<matrix.GetLength(0); i++)		//GetLength(0) returns the number of rows
+// 			{
+// 				for(int j=0; j<matrix.GetLength(1); j++)	//GetLength(1) returns the number of columns
+// 				{
+// 					matrix[i,j] = int.Parse(Console.ReadLine());		// taking input from the user
+// 				}
+// 			}
+// 			int min = matrix[0,0];
+// 			for(int i = 0; i<matrix.GetLength(0); i++)		//GetLength(0) returns the number of rows
+// 			{
+// 				for(int j=0; j<matrix.GetLength(1); j++)	//GetLength(1) returns the number of columns
+// 				{
+// 					if(matrix[i,j] < min)		//if the element is less than the min element
+// 					{
+// 						min = matrix[i,j];		//assign the element to min
+// 					}
+// 				}
+// 			}
+// 			Console.WriteLine("The smallest element of the matrix is :" + min);
+
+// 		}
+
+// 	}
 // }
---
+++ dot-NET/test/tempCodeRunnerFile.cs
@@ -0,0 +1,26 @@
+// //Write a program to find the smallest element from the given array
+
+// using System;
+
+// namespace Smallest{
+//     class Smallest{
+//         static void Main()
+//         {
+//             int[] arr = new int[5];
+//             Console.WriteLine("Enter the elements of the array :");
+//             for(int i = 0; i < arr.Length; i++)
+//             {
+//                 arr[i] = int.Parse(Console.ReadLine());
+//             }
+//             int min = arr[0];
+//             for(int i = 0; i < arr.Length; i++)
+//             {
+//                 if(arr[i] < min)
+//                 {
+//                     min = arr[i];
+//                 }
+//             }
+//             Console.WriteLine("The smallest element of the array is :"+min);
+//         }
+//     }
+// }
