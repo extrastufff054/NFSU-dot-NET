1935bd9ff3f315520fc9153e0cd074855202b6a5
extrastufff054 <extrastuff054@gmail.com>
2023-06-21 12:26:21 +0530

    21-06-2023

--- dot-NET/P5.cs
+++ dot-NET/P5.cs
@@ -1,18 +1,18 @@
-// User input to Rational number
-using System;
-using System.Numerics;
+// // User input to Rational number
+// using System;
+// using System.Numerics;
 
-namespace Rational
-{
-    class Rational
-    {
-        static void Main()
-        {
-            Console.Write("Enter a number :");
-            string str = Console.ReadLine();
+// namespace Rational
+// {
+//     class Rational
+//     {
+//         static void Main()
+//         {
+//             Console.Write("Enter a number :");
+//             string str = Console.ReadLine();
 
             
-        }
-    }
-}
+//         }
+//     }
+// }
 
